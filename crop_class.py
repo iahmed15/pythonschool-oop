8a595b73006eb6dd2a21ca893ec3af31e6c7bf98
iahmed15 <71080736+iahmed15@users.noreply.github.com>
2020-09-25 17:16:27 +0100

    Added a management menu
    Added a management menu to enable us to easily access the different test functions and see the result of calling these functions on a particular crop instance

--- crop_class.py
+++ crop_class.py
@@ -126,12 +126,57 @@ def manual_grow(crop):
     # Grow the crop
     crop.grow(light, water)
 
+
+def display_menu():
+    print("1. Grow manually over 1 day")
+    print("2. Grow automatically over 30 days")
+    print("3. Report status")
+    print("0. Exit test program")
+    print()
+    print("Please select an option from the above menu")
+
+
+def get_menu_choice():
+    option_valid = False
+    while not option_valid:
+        try:
+            choice = int(input("Option Selected: "))
+            if 0 <= choice < 4:
+                option_valid = True
+            else:
+                print("Please enter a valid option")
+        except ValueError:
+            print("Please enter a valid option")
+    return choice
+
+
+def manage_crop(crop):
+    print("This is the crop management program")
+    print()
+    no_exit = True
+    while no_exit:
+        display_menu()
+        option = get_menu_choice()
+        print()
+        if option == 1:
+            manual_grow(crop)
+            print()
+        elif option == 2:
+            auto_grow(crop, 30)
+            print()
+        elif option == 3:
+            print(crop.report())
+            print()
+        elif option == 0:
+            no_exit = False
+            print()
+    print("Thank you for using the crop management program")
+
+
 def main():
+    # Instantiate the class
     new_crop = Crop(1,4,3)
-    print(new_crop.needs())
-    print(new_crop.report())
-    manual_grow(new_crop)
-    print(new_crop.report())
+    manage_crop(new_crop)
 
 
 if __name__ == "__main__":
